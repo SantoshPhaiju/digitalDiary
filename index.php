64066417c30412c8c21198bee98b9d7836910c72
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-22 07:38:57 +0545

    Home page structure and designing done.

--- index.php
+++ index.php
@@ -2,9 +2,63 @@
 
 <main>
 
-    <h1>
-        Welcome to My Digital Diary!
-    </h1>
+    <div class="mainHeading">
+        <h1>
+            Welcome to My Digital Diary!
+        </h1>
+        <button>
+            + Add New Entry
+        </button>
+    </div>
+
+    <div class="myContents">
+        <div class="diaryEntry">
+            <div>
+                Wednesday, 18 October 2023
+            </div>
+            <h2>
+                Today was a good day!
+            </h2>
+            <div class="content">
+                Today I learned about PHP and how to create a simple web application. I also went for a walk in the park
+                and enjoyed the beautiful weather. It was a refreshing da...
+            </div>
+            <button>
+                <a href="#">See More</a>
+            </button>
+            <button>
+                <a href="#">Edit</a>
+            </button>
+            <button>
+                <a href="#">Delete</a>
+            </button>
+
+        </div>
+        <div class="diaryEntry">
+            <div>
+                Wednesday, 18 October 2023
+            </div>
+            <h2>
+                Today was a good day!
+            </h2>
+            <div class="content">
+                Today I learned about PHP and how to create a simple web application. I also went for a walk in the park
+                and enjoyed the beautiful weather. It was a refreshing da...
+            </div>
+            <button>
+                <a href="#">See More</a>
+            </button>
+            <button>
+                <a href="#">Edit</a>
+            </button>
+            <button>
+                <a href="#">Delete</a>
+            </button>
+
+        </div>
+    </div>
+
+
 </main>
 
 
