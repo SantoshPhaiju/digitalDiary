b663aaa5b3c32c40743c84a41a44f08aebebf697
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-28 15:06:19 +0545

    Add header button group and styles for login/register actions

--- header.php
+++ header.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Digital Diary</title>
-    <link rel="stylesheet" href="./css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
@@ -19,4 +19,14 @@
                 <li><a href="index.php">Home</a></li>
             </ul>
         </nav>
+        <div class="buttons">
+            <button>
+                <a href="login.php">Login</a>
+            </button>
+            <button>
+                <a href="register.php">Register</a>
+            </button>
+        </div>
+
+
     </header>
