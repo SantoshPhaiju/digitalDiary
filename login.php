083f6b00ef896ba4031c481e00b16165fad5fb3b
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-28 15:15:47 +0545

    Add login page structure and styles; include login.css for styling

--- login.php
+++ login.php
@@ -1,7 +1,26 @@
 <?php require_once 'header.php'; ?>
 
-<main>
-    
+
+
+<main class="loginPage">
+    <div class="loginContainer">
+
+        <h1>Login to My Digital Diary</h1>
+
+        <form method="post" action="login.php">
+            <div class="formGroup">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="formGroup">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+
+    </div>
+
 </main>
 
 
