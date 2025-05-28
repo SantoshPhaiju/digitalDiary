0501552a2a6bba2931ba1ca0cd556458d8bb1949
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-28 17:51:47 +0545

    Refactor db.php: remove unnecessary whitespace and closing PHP tag; ensure proper connection setup

--- db/db.php
+++ db/db.php
@@ -1,10 +1,8 @@
-<?php 
+<?php
 
 $hostname = "localhost";
 $username = "root";
 $password = "";
 $database = "digitalDiary";
 
-
-
-?>
+$conn = mysqli_connect($hostname, $username, $password, $database);
