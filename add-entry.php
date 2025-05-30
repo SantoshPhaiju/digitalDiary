7f33bb4da368eabe7393965d4f805539d8de0151
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-30 19:37:01 +0545

    Implement session check in add-entry page to redirect unauthenticated users to login

--- add-entry.php
+++ add-entry.php
@@ -1,11 +1,16 @@
 <?php
 require_once 'header.php';
+require_once './db/db.php';
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
 
 ?>
 
 
 <main>
-
     <div class="mainHeading">
         Add Entry Page
     </div>
