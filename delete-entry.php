92bc5710eb534ad47d6336c2662d5cc8f4efbc9d
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-31 20:24:06 +0545

    Add delete-entry.php: implement entry deletion functionality with session validation

---
+++ delete-entry.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+}
+
+require_once './db/db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $entryId = $_GET['id'];
+
+    if (is_numeric($entryId)) {
+        $stmt = $conn->prepare("DELETE FROM diary WHERE id = ? AND user_id = ?");
+        $stmt->bind_param("ii", $entryId, $_SESSION['user_id']);
+    }
+}
+
+
+
+
