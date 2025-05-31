8a36a3fd78e0bebc5c1394f8df84d587f3c41cb2
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-31 14:27:24 +0545

    Refactor save-updated-entry.php: improve session handling and add validation for entry updates

--- save-updated-entry.php
+++ save-updated-entry.php
@@ -1,8 +1,26 @@
 <?php
 
 require_once './db/db.php';
+
 session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $title = trim($_POST['title']);
+    $content = trim($_POST['content']);
+    $entryId = $_GET['id'] ?? null;
+    $userId = $_SESSION['user_id'];
+
+    if ($entryId && is_numeric($entryId)) {
+        if (empty($title) || empty($content)) {
+            header("Location: edit-entry.php?id=$entryId&error=Please fill all the required details");
+        }
+
+    } else {
+        header("Location: index.php");
+    }
+
 }
