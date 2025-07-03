68b169ffca94a2df15583c79dab3134b535259e1
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-07-03 16:15:30 +0545

    Update database connection settings: specify port and set password for MySQL connection

--- db/db.php
+++ db/db.php
@@ -1,8 +1,8 @@
 <?php
 
-$hostname = "localhost";
+$hostname = "localhost:3306";
 $username = "root";
-$password = "";
+$password = "@CoderMan99";
 $database = "digital_diary";
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
